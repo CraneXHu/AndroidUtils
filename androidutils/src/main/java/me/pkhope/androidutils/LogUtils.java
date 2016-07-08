9ecdc97d050f598f67116e96bb8c7493f0aaee6f
pkhope <hhxnbw@163.com>
2016-07-08 23:01:45 +0800

    add LogUtils

--- androidutils/src/main/java/me/pkhope/androidutils/LogUtils.java
+++ androidutils/src/main/java/me/pkhope/androidutils/LogUtils.java
@@ -1,9 +1,54 @@
 package me.pkhope.androidutils;
 
+import android.util.Log;
+
 /**
  * Created by pkhope on 2016/7/4.
  */
 public class LogUtils {
 
+    public static boolean isDebug = true;
 
+    public static void setDebug(boolean isDebug){
+        LogUtils.isDebug = isDebug;
+    }
+
+    public static boolean getDebug(){
+        return isDebug;
+    }
+
+    public static String getClassName(){
+        String tag = new Throwable().getStackTrace()[1].getClassName();
+        return tag;
+    }
+
+    public static void v(String msg){
+        if (isDebug){
+            Log.v(getClassName(),msg);
+        }
+    }
+
+    public static void d(String msg){
+        if (isDebug){
+            Log.d(getClassName(),msg);
+        }
+    }
+
+    public static void i(String msg){
+        if (isDebug){
+            Log.i(getClassName(),msg);
+        }
+    }
+
+    public static void w(String msg){
+        if (isDebug){
+            Log.w(getClassName(),msg);
+        }
+    }
+
+    public static void e(String msg){
+        if (isDebug){
+            Log.e(getClassName(),msg);
+        }
+    }
 }
